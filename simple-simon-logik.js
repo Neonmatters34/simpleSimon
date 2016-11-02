90fb0a869234f78c3f278d57942bb0fd49d559ce
Matthew Garza <matt.garza34@gmail.com>
2016-11-02 15:35:30 -0500

    kCreated a startgame function. Next level function also created which generates a random number which a displays game sequence. Renamed function keyLogging to lightUpButtonOnKeypress.

--- simple-simon-logik.js
+++ simple-simon-logik.js
@@ -7,9 +7,11 @@ var $topRight = $('#triangle-top-right');
 var $bottomLeft = $('#triangle-bottom-left');
 var $bottomRight = $('#triangle-bottom-right');
 var $start = $('#center-square');
+var $round = $('#round');
+var round = 1;
 var whichTriangle = [$topLeft, $topRight, $bottomLeft, $bottomRight];
 var playerSequence= [];
-var gameSequence = [];
+var gameSequence = [1, 3, 2, 4];
 var randomColor;
 var keys = {
     q:81,
@@ -18,55 +20,88 @@ var keys = {
     s:83
 };
 
-$start.click(startGame());
+
+function nextLevel() {
+    $round.text('round ' + round );
+    randomNumber();
+    displaySequence();
+    lightUpButtonOnKeypress();
+}
+
+function displaySequence() {
+    $.each(gameSequence, function(index, element){
+    animateOpacity(element);
+
+    });
+}
+
+function randomNumber() {
+        gameSequence.push(Math.floor((Math.random() * 4) + 1));
+        console.log(gameSequence)
+}
+
+function animateOpacity(toBeAnimated) {
+    var triangleToAnimate;
+    if (typeof toBeAnimated == 'number'){
+        if (toBeAnimated == 1){
+            triangleToAnimate = $topLeft;
+        }else if (toBeAnimated == 2){
+            triangleToAnimate = $topRight;
+        }else if (toBeAnimated == 3){
+            triangleToAnimate = $bottomLeft;
+        }else if (toBeAnimated == 4){
+            triangleToAnimate = $bottomRight
+        }
+    }else {
+        triangleToAnimate = toBeAnimated;
+    }
+    triangleToAnimate.animate({
+            opacity: "1"
+        },
+        {
+            duration: '100',
+            complete: function () {
+                triangleToAnimate.animate({opacity: '.5'}, 300)
+            }
+        });
+
+}
+
+function lightUpButtonOnKeypress() {
+    $(document).keyup(function (e) {
+
+
+        if (e.keyCode === keys.a) {
+            animateOpacity($bottomLeft);
+            playerSequence.push(4);
+            console.log(playerSequence);
+        } else if (e.keyCode === keys.q) {
+            animateOpacity($topLeft);
+            playerSequence.push(1);
+            console.log(playerSequence);
+        } else if (e.keyCode === keys.w) {
+            animateOpacity($topRight);
+            playerSequence.push(2);
+            console.log(playerSequence);
+        } else if (e.keyCode === keys.s) {
+            animateOpacity($bottomRight);
+            playerSequence.push(3);
+            console.log(playerSequence);
+        }
+    })
+}
+
+
 
 function startGame() {
 
-        function randomNumber() {
-            for (var i = 0; i < playerSequence.length; i++) {
-                randomColor = Math.floor((Math.random() * 4) + 1);
-                gameSequence.push(randomColor);
-                console.log(gameSequence)
-            }
-        }
+    nextLevel();
 
-        randomNumber();
-        function animateOpacity(toBeAnimated) {
-            toBeAnimated.animate({
-                    opacity: "1"
-                },
-                {
-                    duration: '100',
-                    complete: function () {
-                        toBeAnimated.animate({opacity: '.5'}, 300)
-                    }
-                });
-        }
-
-
-        function keyLogging() {
-            $(document).keyup(function (e) {
-
-
-                if (e.keyCode === keys.a) {
-                    animateOpacity($bottomLeft);
-                    playerSequence.push(4);
-                    console.log(playerSequence);
-                } else if (e.keyCode === keys.q) {
-                    animateOpacity($topLeft);
-                    playerSequence.push(1);
-                    console.log(playerSequence);
-                } else if (e.keyCode === keys.w) {
-                    animateOpacity($topRight);
-                    playerSequence.push(2);
-                    console.log(playerSequence);
-                } else if (e.keyCode === keys.s) {
-                    animateOpacity($bottomRight);
-                    playerSequence.push(3);
-                    console.log(playerSequence);
-                }
-            })
-        }
-    keyLogging();
     }
+
+
+$start.click(function (){
+    startGame();
+    $('#start').hide();
+});
 
